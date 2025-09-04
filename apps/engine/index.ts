d23a534361512fe1f0b4c3cab6663a092e374763
jassBawa <jasspreetsbawa@gmail.com>
2025-09-04 22:13:31 +0530

    exness v3 basic setup

---
+++ apps/engine/index.ts
@@ -0,0 +1,19 @@
+import { createClient } from "redis";
+
+const client = createClient();
+
+await client.connect();
+
+while(true){
+    const result = await client.xRead({
+        key: "trades",
+        id: '$' // only new message
+    }, {
+        BLOCK: 0 // wait forever
+    })
+
+    console.log('result', result)
+    if(result){
+
+    }
+}
