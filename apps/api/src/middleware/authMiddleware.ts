f83349505043be97b50fa27f28af5723e87e67af
jassBawa <jasspreetsbawa@gmail.com>
2025-09-06 23:01:31 +0530

    cookie middleware added

--- apps/api/src/middleware/authMiddleware.ts
+++ apps/api/src/middleware/authMiddleware.ts
@@ -1,6 +1,34 @@
-import type { NextFunction, Request, Response } from "express";
+import type { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
+export async function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const authCookie = req.cookies['token'];
 
-export async function authMiddleware(req: Request, res: Response, next: NextFunction){
-    const authCookie = req.cookies['token']
+    if (!authCookie) {
+      res.status(404).json({ message: 'Cookie not found' });
+      return;
+    }
+
+    const isVerified = jwt.verify(authCookie, process.env.JWT_SECRET!);
+
+    if (!isVerified) {
+      res.cookie('token', {});
+      res.status(400).json({ message: 'Invalid Cookie' });
+      return;
+    }
+
+    next();
+
+    console.log(authCookie);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 }
+
+
--- apps/api/src/routes/auth.routes.ts
+++ apps/api/src/routes/auth.routes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import { signInVerify, signupHandler } from '../controllers/auth.controller';
+import { authMiddleware } from '../middleware/authMiddleware';
 
 const authRouter = express();
 
 authRouter.post('/signup', signupHandler);
 authRouter.get('/signin/verify', signInVerify)
+
+// protected route for testing
+authRouter.get('/protected-route-test', authMiddleware ,(req, res) => {
+    console.log("hello sir")
+    res.json({})
+})
 
 export default authRouter;
